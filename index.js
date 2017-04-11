bef7548188291427daf3da851438ad080ee3e379
nik-john <nikhil.john.88@gmail.com>
2017-04-12 02:43:27 +0530

    Add wurfl

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 /* jshint node: true */
+
 'use strict';
 
 module.exports = {
-  name: 'ember-wurfl'
+  name: 'ember-wurfl',
+  contentFor : function(type, config) {
+    var content = '';
+    if (type === 'body') {
+      var src = '//wurfl.io/wurfl.js';
+    }
+    content = '<script type="text/javascript" src="' + src + '"></script>';
+    return content;
+  }
 };
